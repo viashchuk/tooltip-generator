78c688ba73f190c86950881c0595b8fabc533dbe
Victoriya <victoriya_yashchuk@mail.ru>
2020-06-02 11:02:57 +0200

    ✂️ Move PurgeCSS to tailwind.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,12 @@
 const tailwindcss = require('tailwindcss')
 const autoprefixer = require('autoprefixer')
-const purgecss = require('@fullhuman/postcss-purgecss')
 
-module.exports = {
-  plugins: [
-    tailwindcss('./tailwind.config.js'),
-    autoprefixer({
-      add: true,
-      grid: true
-    }),
-    process.env.NODE_ENV === 'production' ? purgecss({
-      content: [
-        './src/**/*.html',
-        './src/**/*.vue'
-      ]
-    }) : ''
-  ]
-}
+const plugins = [
+  tailwindcss('./tailwind.config.js'),
+  autoprefixer({
+    add: true,
+    grid: true
+  })
+]
+
+module.exports = { plugins }
